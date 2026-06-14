2e26e3983ad8d3d89e4d76200399b97d9873466d
Nuillexe <159359967+Nuillexe@users.noreply.github.com>
2026-06-14 17:16:44 -0300

    Revert "Alteração da classe Comentario e UI"

--- src/Comentario.java
+++ src/Comentario.java
@@ -7,36 +7,24 @@ public class Comentario {
     private int id;
     private String autor;
     private String texto;
+
     private Comentario pai;
+
     private LinkedList<Comentario> respostas;
 
-    public Comentario(Comentario pai) {
-        this.pai = pai;
-    }
-
-    public Comentario(String autor, String texto, Comentario pai){
+    public Comentario(
+            String autor,
+            String texto,
+            Comentario pai
+    ){
         this.autor = autor;
         this.texto = texto;
         this.pai = pai;
+
         this.respostas = new LinkedList<>();
+
         this.id = proximoId++;
     }
 
-    public void setId(int id) { this.id = id; }
-    public void setAutor(String autor) { this.autor = autor; }
-    public void setTexto(String texto) { this.texto = texto; }
-
-    public int getId() { return id; }
-    public String getAutor() { return autor; }
-    public String getTexto() { return texto; }
-    public Comentario getPai() { return pai; }
-
-    // Métodos adicionados para que a classe UI consiga ler as respostas:
-    public LinkedList<Comentario> getRespostas() {
-        return respostas;
-    }
-
-    public int getQuantidadeRespostas() {
-        return respostas != null ? respostas.size() : 0;
-    }
+    // getters e setters
 }
